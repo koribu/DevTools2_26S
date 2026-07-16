1588eab2028a32fe0b375bc72f5e9f552bc6a137
koribu <snnklp@gmail.com>
2026-07-16 15:43:03 -0400

    Player behavior with movement is created

---
+++ DevTool2-2026 project/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public InputActionAsset playerController;
+
+    private InputAction moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World from Start Func");
+
+        moveInput = playerController.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Move input is: " + moveInput.ReadValue<Vector2>());
+
+        float horizontalInput = moveInput.ReadValue<Vector2>().x;
+        float verticalInput = moveInput.ReadValue<Vector2>().y;
+
+        transform.position = new Vector3(transform.position.x + horizontalInput * speed * Time.deltaTime, 
+                                        transform.position.y + verticalInput * speed * Time.deltaTime, 
+                                        transform.position.z);
+    }
+}
